17d0abc5255dfd5b5d3b3014aeea1c5b3a418161
Deepali Verma <deepaliverma064@gmail.com>
2025-04-07 22:16:08 +0530

    url added

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 // CORS Configuration
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173","https://mailcanvasbydeepali.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,

5b1c3e89cd4ca509fac16227c9f4cfa7d9b014cd
Deepali Verma <deepaliverma064@gmail.com>
2025-04-07 21:32:06 +0530

    new folder added

--- server/server.js
+++
@@ -1,8 +0,0 @@
-import app from "./app.js"; 
-import { config } from 'dotenv';
-
-config({ path: './config/config.env' });
-
-app.listen(process.env.PORT || 5000, () =>
-  console.log(`Server is running on http://localhost:${process.env.PORT}`)
-);
